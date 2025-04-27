1f56932be73a581a3861841368f481e519c19899
Illia S <illia.shakun.k@gmail.com>
2025-04-27 02:17:59 +0200

    remove use memo

--- src/pages/utils/PDFDisplay.tsx
+++ src/pages/utils/PDFDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import PDFViewer, { PDFViewerProps } from './PDFViewer';
 
 const PDFDisplay: React.FC<PDFViewerProps> = (props) => {
@@ -14,8 +14,7 @@ const PDFDisplay: React.FC<PDFViewerProps> = (props) => {
             ? `${window.location.origin}/slide.pdf`
             : props.pdfUrl;
     }, [isDevelopment, props.pdfUrl]);
-    
-    return (
+      return (
         <PDFViewer
             {...props}
             pdfUrl={pdfUrl}
@@ -23,21 +22,4 @@ const PDFDisplay: React.FC<PDFViewerProps> = (props) => {
     );
 };
 
-// Use React.memo to prevent re-rendering when props don't change
-export default memo(PDFDisplay, (prevProps, nextProps) => {
-    // Custom comparison function that determines if component should update
-    // Return true if the props are equal (meaning don't re-render)
-    
-    // Compare pdfUrl specifically since it's the most important prop
-    if (prevProps.pdfUrl !== nextProps.pdfUrl) {
-        return false; // Props are different, should re-render
-    }
-    
-    // Check other common props that might affect rendering
-    if (prevProps.visiblePagePercentage !== nextProps.visiblePagePercentage) {
-        return false;
-    }
-    
-    // For any other props, we consider them equal to prevent re-renders
-    return true;
-});
+export default PDFDisplay;
